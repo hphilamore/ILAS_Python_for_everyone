ca6531876a41248fe8908f7f977b6ffb7d8e7c1d
hphilamore <hemmaphilamore@gmail.com>
2018-06-04 13:07:36 +0900

    saucer spinning and shots registered as list

--- sample_data/space_game.py
+++ sample_data/space_game.py
@@ -34,6 +34,9 @@ if random.randrange(0,2) == 0:
     monster_vel[x] *= -1
 if random.randrange(0,2) == 0:
     monster_vel[y] *= -1
+    
+# 2.2 List to store shots 
+shots = []
  
 
 # 3. Launch a game window
@@ -42,6 +45,7 @@ window = pygame.display.set_mode((800, 600))
 background = pygame.image.load("../img/space.jpg").convert()
 #player = pygame.image.load("../img/rabbit.png")
 saucer = pygame.image.load("../img/saucer.png")
+fire = pygame.image.load("../img/fire.png")
 #spaceship = pygame.image.load("../img/spaceship.png")
 #spaceship.set_colorkey(white)
 
@@ -85,6 +89,13 @@ while True:
             
     # 4.1.2 : Mouse position
     mouse_pos = pygame.mouse.get_pos()
+    
+    # 4.1.3 : Mouse click
+    if event.type==pygame.MOUSEBUTTONDOWN:
+        mouse_click = True
+    else:
+        mouse_click = False
+
         
     # 4.2 Calculations  
     # 4.2.1 Reverse direction of travel if edge is reached
@@ -104,15 +115,37 @@ while True:
 #                       -(mouse_pos[y]-(saucer_pos[y]+39)))
     angle = math.atan2(-(mouse_pos[y]-(saucer_pos[y]+39)),
                         (mouse_pos[x]-(saucer_pos[x]+80)))
-    angle = math.degrees(angle)-90
-    print(angle)
-    print(angle)
-    print(angle)
+    #angle = math.degrees(angle)-90
+    angle -= math.pi/2
+
     
     # 4.2.4 Rotate the saucer
-    saucer_r = pygame.transform.rotate(saucer, angle)
+    saucer_r = pygame.transform.rotate(saucer, math.degrees(angle))
     
-    
+    # 4.2.5 Fire a shot
+    if mouse_click:
+        
+        # if the mouse is clicked, a shot is added to the list of shots
+        shot_pos = [saucer_pos[x]+80, saucer_pos[y]+39]
+        shots.append([angle, shot_pos])
+        
+        # iterate over all shots, giving each one a number
+        for n, shot in enumerate(shots):
+            speed = 10
+            shot_vel = [math.cos(shot[0])*speed, 
+                        math.sin(shot[0])*speed]
+            shot[1][x] += shot_vel[x]
+            shot[1][y] += shot_vel[y]
+            
+            # if the fireball leaves the screen, remove it from the list of shots
+            if (shot[1][x] > 800 + 94 or 
+                shot[1][x] < -94 or 
+                shot[1][y] > 600 + 94 or 
+                shot[1][y] < -94):
+                shots.pop(n)
+                
+                
+            
     
         
 #    elif event.type == pygame.MOUSEBUTTONDOWN:
