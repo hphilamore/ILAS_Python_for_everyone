339a1560600498f45e91910a2750a0c89f0af4dd
hphilamore <hemmaphilamore@gmail.com>
2018-07-03 16:30:26 +0900

    new update

--- sample_data/pytest_examples/calculator_example.py
+++ sample_data/pytest_examples/calculator_example.py
@@ -1,35 +1,36 @@
+import numbers
+
 def test_calculator_add_returns_correct_result():
     result = calc_add(2,2)
     assert result == 4
 
 
-# def calc_add(x,y):
-# 	pass
-
-	# return x+y
-
-    # if isinstance(x, number_types) and isinstance(y, number_types):
-    #             return x + y
-    #         else:
-    #             raise ValueError("Non-numeric input given")
-
-
-
-
-
-# def test_calculator_returns_error_message_if_both_args_not_numbers():
-# 	try:
-#             calc_add("two", "three")
-
-#         except ValueError:
-#             print("Exception caught")
-#             assert True, "Fail: ValueError exception not caught"
-
-#         except:
-#             assert False, "Fail: Exception other than ValueError caught"
-
-#         else: 
-#             assert False, "Fail: No exception caught"
+#def calc_add(x,y):
+#     # pass
+#
+#	#return x+y
+#
+#    if isinstance(x, numbers.Number) and isinstance(y, numbers.Number):
+#        return x + y
+#    else:
+#        raise ValueError("Non-numeric input given")
+#
+#
+#
+#
+#
+#def test_calculator_returns_error_message_if_both_args_not_numbers():
+#    try:
+#        calc_add("two", "three")
+#    except ValueError:
+#        print("Exception caught")
+#        assert True, "Fail: ValueError exception not caught"
+#
+#    except:
+#        assert False, "Fail: Exception other than ValueError caught"
+#
+#    else: 
+#        assert False, "Fail: No exception caught"
 
 
 
---
+++ sample_data/test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 26 17:31:33 2018
+
+@author: hemma
+"""
+a = np.loadtxt('sample_data/sample_data_seminar10.dat')
+
+a = int(a[0][0])
+
+a = 1 / a
+
+print(a)
